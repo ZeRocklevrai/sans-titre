fb99d5d1807f26cbb114aca9c6a2b7d29580bfe3
ZeRocklevrai <103418883+ZeRocklevrai@users.noreply.github.com>
2022-05-30 10:48:37 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,54 @@
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) < -200) {
+        basic.showLeds(`
+            # # # # .
+            # # . . .
+            # . # . .
+            # . . # .
+            . . . . #
+            `)
+    } else if (input.acceleration(Dimension.X) > 200) {
+        basic.showLeds(`
+            . # # # #
+            . . . # #
+            . . # . #
+            . # . . #
+            # . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.Y) < 200) {
+        if (input.acceleration(Dimension.X) < -200) {
+            basic.showLeds(`
+                . . . . #
+                # . . # .
+                # . # . .
+                # # . . .
+                # # # # .
+                `)
+        } else if (input.acceleration(Dimension.X) > 200) {
+            basic.showLeds(`
+                # . . . .
+                . # . . #
+                . . # . #
+                . . . # #
+                . # # # #
+                `)
+        } else {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                `)
+        }
+    }
+})
